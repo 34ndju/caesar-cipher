667e9456c2506ed803586d393f74ab0c3d6d1d0e
34ndju <34ndju@gmail.com>
2017-10-18 01:25:34 -0500

    drose lol

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -33,7 +33,7 @@ letter_distribution = {
     'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0,
     'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
 }
-wiki_link = "https://github.com/ASankaran/DataStructures/blob/master/RedBlackTree/redblacktree.c"
+wiki_link = "https://en.wikipedia.org/wiki/Derrick_Rose"
 connection = urllib2.urlopen(wiki_link)
 html = connection.read()
 connection.close()

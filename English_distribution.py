313bd569aa2affaedce0cae8631387be49cc7d15
34ndju <34ndju@gmail.com>
2017-10-18 00:06:21 -0500

    refactored english distribution to normalize

--- English_distribution.py
+++ English_distribution.py
@@ -8,9 +8,23 @@ class English_distribution:
             'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0,
             'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
         }
+        self.wiki_link = "https://en.wikipedia.org/wiki/Donald_Glover"
 
-    def get_distribution_from_wikipedia(self, wiki_link):
-        connection = urllib2.urlopen(wiki_link)
+
+    def normalize_letter_distribution(self, full_distribution):
+        max = 0
+
+        for key, value in full_distribution.iteritems():
+            if value > max:
+                max = value
+
+        for key, value in full_distribution.iteritems():
+            full_distribution[key] = float(full_distribution[key]) / max
+
+        return full_distribution
+
+    def get_full_english_distribution_from_wikipedia(self):
+        connection = urllib2.urlopen(self.wiki_link)
         html = connection.read()
         connection.close()
         soup = BeautifulSoup(html, 'html5lib')
@@ -22,18 +36,4 @@ class English_distribution:
                 self.letter_distribution[corpus[i].lower()]+=1
 
 
-        return self.letter_distribution
-
-
-    def dist_sample(self):
-        max = 0
-        full_distribution = self.get_distribution_from_wikipedia('https://en.wikipedia.org/wiki/Donald_Glover')
-
-        for key, value in full_distribution.iteritems():
-            if value > max:
-                max = value
-
-        for key, value in full_distribution.iteritems():
-            full_distribution[key] = float(full_distribution[key]) / max
-
-        return full_distribution
+        return self.normalize_letter_distribution(self.letter_distribution)
--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,4 +1,9 @@
-import English_distribution
+from English_distribution import English_distribution
 
 ed = English_distribution()
-print ed.dist_sample()
+dist = ed.get_full_english_distribution_from_wikipedia()
+
+print dist
+print ord('a')
+
+alpha_order = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'x', 'z']

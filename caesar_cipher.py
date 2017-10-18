d8bac0b64bc0921d27b871de6df765c3fb96ecff
34ndju <34ndju@gmail.com>
2017-10-18 01:23:11 -0500

    distance between shifts

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,6 @@
 import urllib2
 from bs4 import BeautifulSoup
+import math
 from English_distribution import English_distribution
 
 ed = English_distribution()
@@ -21,19 +22,26 @@ def shift_string(str, shift_amt):
 
     return translated
 
-str = "The specific contributions of this paper are as follows: we trained one of the largest convolutional neural networks to date on the subsets of ImageNet used in the ILSVRC-2010 and ILSVRC-2012 competitions [2] and achieved by far the best results ever reported on these datasets. We wrote a highly-optimized GPU implementation of 2D convolution and all the other operations inherent in training convolutional neural networks, which we make available publicly1 . Our network contains a number of new and unusual features which improve its performance and reduce its training time, which are detailed in Section 3. The size of our network made overfitting a significant problem, even with 1.2 million labeled training examples, so we used several effective techniques for preventing overfitting, which are described in Section 4. Our final network contains five convolutional and three fully-connected layers, and this depth seems to be important: we found that removing any convolutional layer"
+def get_vector_distance_of_letters(eng_dist, sample_dist, shift_guess):
+    sum_of_squares = 0
+    for key, value in eng_dist.iteritems():
+        sum_of_squares += (sample_dist[shift_char(key, shift_guess)] - value)**2
+    return math.sqrt(sum_of_squares)
+
 
 letter_distribution = {
     'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0, 'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0, 'k': 0, 'l': 0, 'm': 0,
     'n': 0, 'o': 0, 'p': 0, 'q': 0, 'r': 0, 's': 0, 't': 0, 'u': 0, 'v': 0, 'w': 0, 'x': 0, 'y': 0, 'z': 0
 }
-wiki_link = "https://en.wikipedia.org/wiki/LeBron_James"
+wiki_link = "https://github.com/ASankaran/DataStructures/blob/master/RedBlackTree/redblacktree.c"
 connection = urllib2.urlopen(wiki_link)
 html = connection.read()
 connection.close()
 soup = BeautifulSoup(html, 'html5lib')
 corpus = soup.text
-corpus = shift_string(corpus,3)
+corpus = shift_string(corpus,9)
+
+print corpus
 
 for symbol in corpus:
     if symbol.lower() in letter_distribution:
@@ -41,4 +49,11 @@ for symbol in corpus:
 
 print ed.normalize_letter_distribution(letter_distribution)
 
-print corpus
+shift_vector_distances = []
+for i in range(26):
+    shift_vector_distances.append(get_vector_distance_of_letters(dist, ed.normalize_letter_distribution(letter_distribution), i))
+
+print shift_vector_distances
+print shift_vector_distances.index(min(shift_vector_distances))
+
+#print corpus
